09bef08d8300c02d7a1be90204a55948244bcceb
wilman orellano heredia <wilmanjunior2001@gmail.com>
2021-02-10 16:30:36 -0500

    se realizo el cambio de los id por los uuid, ademas de realizar la autenticacion de los usuarios con devise y se implemento el diseño al login y al signup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # devise_for :users, 
+  devise_for :users, skip: [:sessions], path_names:{sign_up:'signup', password: 'secret', confirmation: 'verification', unlock: 'unblock'}
+  as :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   get 'home/index'
   root to:'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
---
+++ db/migrate/20210210164702_enable_uuid.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+class EnableUuid < ActiveRecord::Migration[5.2]
+  def change
+    enable_extension 'pgcrypto'
+  end
+end
